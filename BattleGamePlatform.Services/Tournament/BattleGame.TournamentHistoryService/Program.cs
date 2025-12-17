4090fc2a85c22efd169794e7fcc25fd626eeb540
danielleit241 <danielleee241@gmail.com>
2025-12-17 14:46:00 +0700

    feat: add CQRS infra and DI for tournament services
    Refactor project structure to include Apis, Boostrapping, and Services folders. Register repository interfaces and implementations for tournaments, participants, rounds, and matches with DI in both TournamentHistoryService and TournamentService. Integrate MediatR for CQRS support. Move OpenAPI setup for better initialization order and map custom tournament API endpoints. No changes to business logic or API contracts.

--- BattleGamePlatform.Services/Tournament/BattleGame.TournamentHistoryService/Program.cs
+++ BattleGamePlatform.Services/Tournament/BattleGame.TournamentHistoryService/Program.cs
@@ -1,17 +1,27 @@
 using BattleGame.MessageBus;
 using BattleGame.Shared;
 using BattleGame.Shared.Common;
+using BattleGame.TournamentHistoryService.Apis;
+using BattleGame.TournamentHistoryService.CQRSServices.Tournament.Query;
+using BattleGame.TournamentHistoryService.Repositories;
+using BattleGame.TournamentHistoryService.Repositories.Interfaces;
 using BattleGamePlatform.ServiceDefaults;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHttpContextAccessor();
 builder.AddServiceDefaults();
 builder.AddRateLimit();
-builder.Services.AddOpenApi();
 builder.AddMongoDb(Const.TournamentDatabase + "Read");
 builder.Services.AddMassTransitWithRabbitMq(builder.Configuration);
+builder.Services.AddScoped<ITournamentRepository, TournamentRepository>();
+builder.Services.AddScoped<ITournamentParticipantRepository, TournamentParticipantRepository>();
+builder.Services.AddScoped<ITournamentRoundRepository, TournamentRoundRepository>();
+builder.Services.AddScoped<ITournamentMatchRepository, TournamentMatchRepository>();
 
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(typeof(GetAllTournamentQuery).Assembly));
 
+builder.Services.AddOpenApi();
 var app = builder.Build();
 app.MapDefaultEndpoints();
 if (app.Environment.IsDevelopment())
@@ -23,5 +33,5 @@ app.UseRateLimiter();
 app.UseMiddleware<GlobalExceptionsMiddleware>();
 app.UseAuthentication();
 app.UseAuthorization();
-
+app.MapTournamentApis();
 app.Run();
--- BattleGamePlatform.Services/Tournament/BattleGame.TournamentService/Program.cs
+++ BattleGamePlatform.Services/Tournament/BattleGame.TournamentService/Program.cs
@@ -4,6 +4,8 @@ using BattleGame.Shared.Common;
 using BattleGame.TournamentService.Apis;
 using BattleGame.TournamentService.CQRSServices.Tournament.Command;
 using BattleGame.TournamentService.Infrastructure.Data;
+using BattleGame.TournamentService.Repositories;
+using BattleGame.TournamentService.Repositories.Interfaces;
 using BattleGamePlatform.DatabaseMigrationHelpers;
 using BattleGamePlatform.ServiceDefaults;
 
@@ -14,6 +16,10 @@ builder.AddRateLimit();
 builder.AddNpgsqlDb<TournamentWriteDbContext>(Const.TournamentDatabase + "Write");
 builder.AddJwtConfiguration(builder.Configuration);
 builder.Services.AddMassTransitWithRabbitMq(builder.Configuration);
+builder.Services.AddScoped<ITournamentRepository, TournamentRepository>();
+builder.Services.AddScoped<ITournamentParticipantRepository, TournamentParticipantRepository>();
+builder.Services.AddScoped<ITournamentRoundRepository, TournamentRoundRepository>();
+builder.Services.AddScoped<ITournamentMatchRepository, TournamentMatchRepository>();
 
 builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssembly(typeof(RegisterTournamentHandler).Assembly));
